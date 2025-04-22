f094f8f1792994faf7dadea9bb87ed48d0d04e4a
whiteboy6942 <122088801+whiteboy6942@users.noreply.github.com>
2025-04-22 22:06:39 +0100

    Update server_graphql.py

--- graphql/server_graphql.py
+++ graphql/server_graphql.py
@@ -1,14 +1,25 @@
-from flask import Flask
-from flask_graphql import GraphQLView
-from schema import schema  # <-- agora sim está correto
+from flask import Flask, request, jsonify
+from schema import schema
 
 app = Flask(__name__)
 
-app.add_url_rule(
-    "/graphql",
-    view_func=GraphQLView.as_view("graphql", schema=schema, graphiql=True)
-)
+@app.route("/graphql", methods=["POST"])
+def graphql_server():
+    data = request.get_json()
+    result = schema.execute(
+        data.get("query"),
+        variable_values=data.get("variables")
+    )
 
+    response = {}
+    if result.errors:
+        response['errors'] = [str(error) for error in result.errors]
+    if result.data:
+        response['data'] = result.data
+
+    return jsonify(response)
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002)
+
+
 
