151277fe8ffc8377b57ffe976c25c2d2d0097970
whiteboy6942 <122088801+whiteboy6942@users.noreply.github.com>
2025-04-22 22:42:41 +0100

    Delete soap/cliente_soap.py

--- soap/cliente_soap.py
+++
@@ -1,9 +0,0 @@
-from zeep import Client
-
-client = Client("http://127.0.0.1:8000/?wsdl")
-
-nome = input("Nome do produto a procurar: ")
-resultado = client.service.procurar_produto(nome)
-print("Resultado:", resultado)
-
-
