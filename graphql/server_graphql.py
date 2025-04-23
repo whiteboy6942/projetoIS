a7cab12de77451d3a8618a6ee1822c43d5d173b3
whiteboy6942 <122088801+whiteboy6942@users.noreply.github.com>
2025-04-23 01:07:06 +0100

    Update server_graphql.py

--- graphql/server_graphql.py
+++ graphql/server_graphql.py
@@ -1,16 +1,25 @@
+#Importações principais
 from flask import Flask, request, jsonify
-from schema import schema
+from schema import schema  # Este é o ficheiro onde defines os tipos, queries e mutations
 
+# Criação da app Flask
 app = Flask(__name__)
+
+
+# Endpoint para tratar as requisições GraphQL
 
 @app.route("/graphql", methods=["POST"])
 def graphql_server():
+    #Recebe a query enviada pelo cliente (em JSON)
     data = request.get_json()
+
+    # Executa a query ou mutation com a 'schema' definida no ficheiro schema.py
     result = schema.execute(
         data.get("query"),
         variable_values=data.get("variables")
     )
 
+    # Prepara a resposta JSON com erros ou dados
     response = {}
     if result.errors:
         response['errors'] = [str(error) for error in result.errors]
@@ -18,8 +27,9 @@ def graphql_server():
         response['data'] = result.data
 
     return jsonify(response)
+
+
+# Arrancar o servidor na porta 5002
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002)
-
-
-
